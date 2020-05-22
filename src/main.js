55e459e7fd9f988294d6e43453e7bf078f7bca47
Furkan Tezeren <emintezeren@gmail.com>
2020-05-23 00:58:07 +0300

    Optimized window to keep running in the background on cmd+w on mac

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+let willExitApp = false;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
   app.quit();
 }
 
+let mainWindow;
+
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
     },
@@ -22,10 +26,23 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   
+  
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
+  
+  mainWindow.on('close', (e) => {
+    if (!willExitApp) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+  });
+  mainWindow.on('ready-to-show', () => {
+    if (!willExitApp){
+      mainWindow.show();
+    }
+  })
 
-  app.dock.setIcon( __dirname + '/assets/clock.ico');
+  app.dock.setIcon(__dirname + '/assets/clock.ico');
 };
 
 // This method will be called when Electron has finished
@@ -34,16 +51,18 @@ const createWindow = () => {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', (e) => {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  e.preventDefault();
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+app.on('before-quit', () => (willExitApp = true));
+
 app.on('activate', () => {
+  mainWindow.show();
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
